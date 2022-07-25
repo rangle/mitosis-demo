fbd902debe5a5bb618e388293ab0ea3ba85cd2b7
Michael <michaelmrowetz@gmail.com>
2022-07-24 21:45:22 -0400

    start

---
+++ mitosis.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  files: "src/**",
+  targets: [
+    "react",
+    // still unsupported
+    // 'qwik',
+    // 'builder',
+    "vue",
+    "html",
+    // TO-DO: fix error causing svelte output not to work
+    // 'svelte',
+    "solid",
+    "angular",
+    "webcomponent",
+  ],
+};
